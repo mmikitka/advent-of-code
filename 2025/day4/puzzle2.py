791d5a28b1b6c0805c0424abca2479476dcac51a
Matt Mikitka <matt.mikitka@valencraft.com>
2025-12-30 21:53:53 -0500

    Complete day4

--- 2025/day4/puzzle2.py
+++ 2025/day4/puzzle2.py
@@ -34,43 +34,44 @@ def process_rows(rows):
         row = rows[row_idx]
         num_cells = len(row)
         for cell_idx in range(num_cells):
-            adjacent_rolls = 0
+            if rows[row_idx][cell_idx] == CELL_ROLL:
+                adjacent_rolls = 0
 
-            if cell_idx > 0:
-                # This cell has adjacent left column
+                if cell_idx > 0:
+                    # This cell has adjacent left column
+                    if row_idx > 0:
+                        # Top row has no top-left neighbour
+                        adjacent_rolls += (1 if rows[row_idx - 1][cell_idx - 1] != CELL_EMPTY else 0)
+                    if row_idx < num_rows - 1:
+                        # Bottom row has no bottom-left neighbour
+                        adjacent_rolls += (1 if rows[row_idx + 1][cell_idx - 1] != CELL_EMPTY else 0)
+
+                    adjacent_rolls += (1 if rows[row_idx][cell_idx - 1] != CELL_EMPTY else 0)
+
+                if cell_idx < num_cells - 1:
+                    # This cell has adjacent right column
+                    if row_idx > 0:
+                        # Top row has no top-right neighbour
+                        adjacent_rolls += (1 if rows[row_idx - 1][cell_idx + 1] != CELL_EMPTY else 0)
+                    if row_idx < num_rows - 1:
+                        # Bottom row has no bottom-right neighbour
+                        adjacent_rolls += (1 if rows[row_idx + 1][cell_idx + 1] != CELL_EMPTY else 0)
+
+                    adjacent_rolls += (1 if rows[row_idx][cell_idx + 1] != CELL_EMPTY else 0)
+
+                # Middle column
                 if row_idx > 0:
-                    # Top row has no top-left neighbour
-                    adjacent_rolls += (rows[row_idx - 1][cell_idx - 1] != CELL_ROLL)
-                elif row_idx < num_rows - 1:
-                    # Bottom row has no bottom-left neighbour
-                    adjacent_rolls += (rows[row_idx + 1][cell_idx - 1] != CELL_ROLL)
+                    # Top row has no top neighbour
+                    adjacent_rolls += (1 if rows[row_idx - 1][cell_idx] != CELL_EMPTY else 0)
 
-                adjacent_rolls += (rows[row_idx][cell_idx - 1] != CELL_ROLL)
+                if row_idx < num_rows - 1:
+                    # Bottom row has no bottom neighbour
+                    adjacent_rolls += (1 if rows[row_idx + 1][cell_idx] != CELL_EMPTY else 0)
 
-            if cell_idx < num_cells - 1:
-                # This cell has adjacent right column
-                if row_idx > 0:
-                    # Top row has no top-right neighbour
-                    adjacent_rolls += (rows[row_idx - 1][cell_idx + 1] != CELL_ROLL)
-                elif row_idx < num_rows - 1:
-                    # Bottom row has no bottom-right neighbour
-                    adjacent_rolls += (rows[row_idx + 1][cell_idx + 1] != CELL_ROLL)
-
-                adjacent_rolls += (rows[row_idx][cell_idx + 1] != CELL_ROLL)
-
-            # Middle column
-            if row_idx > 0:
-                # Top row has no top neighbour
-                adjacent_rolls += (rows[row_idx - 1][cell_idx] != CELL_ROLL)
-
-            if row_idx < num_rows - 1:
-                # Bottom row has no bottom neighbour
-                adjacent_rolls += (rows[row_idx + 1][cell_idx] != CELL_ROLL)
-
-            if adjacent_rolls < 4:
-                # Mark the roll for removal on next iteration
-                rows[row_idx][cell_idx] = CELL_REMOVABLE
-                rolls_removable += 1
+                if adjacent_rolls < 4:
+                    # Mark the roll for removal on next iteration
+                    rows[row_idx][cell_idx] = CELL_REMOVABLE
+                    rolls_removable += 1
 
             # Delete "removable" cells from rows 1..N
             if row_idx > 0:
